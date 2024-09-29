90ae655a39a0de22275a3a2f74e71c273a57401b
Frm2003 <felipper475@gmail.com>
2024-09-29 11:32:38 -0300

    refatoração da pasta de contexts

---
+++ src/components/contexts/listaContext.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import Lista from '@/lib/classeLista';
+
+interface ListaContextType {
+    lista: Lista;
+}
+
+const ListaContext = createContext<ListaContextType | undefined>(undefined);
+
+export const ListaProvider: React.FC<{ children: ReactNode }> = ({
+    children,
+}) => {
+    const [lista] = useState<Lista>(new Lista());
+
+    return (
+        <ListaContext.Provider value={{ lista }}>
+            {children}
+        </ListaContext.Provider>
+    );
+};
+
+export const useList = (): ListaContextType => {
+    const context = useContext(ListaContext);
+    if (context === undefined) {
+        throw new Error('useList must be used within a ListaProvider');
+    }
+    return context;
+};
