330c218dff34e18517fca200e179590dda878220
Frm2003 <felipper475@gmail.com>
2024-10-12 15:13:47 -0300

    alteração nas pastas de componentes

--- src/components/contexts/listaContext.tsx
+++ src/components/contexts/listaContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { createContext, useContext, useState, ReactNode } from 'react';
 import Lista from '@/lib/classeLista';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 interface ListaContextType {
     lista: Lista;
